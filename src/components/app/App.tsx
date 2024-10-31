01ed7be05148edbcf7a7155ad3a55165e40e5969
Inna Dmytrenko <inna.shalun@gmail.com>
2024-10-31 15:20:36 +0100

    small fix

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import { getTheme } from '../common/theme/theme';
+
 import { Header } from '../common/components/Header/Header';
 import { Main } from './Main';
 
